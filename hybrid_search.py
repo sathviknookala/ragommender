1ec0052d7a6f29a7da6263751eca343bc003f378
Sathvik Nookala <sathviknookala@neuralads.ai>
2025-09-24 15:18:26 -0400

    changes

--- hybrid_search.py
+++ hybrid_search.py
@@ -1,9 +1,14 @@
 from sklearn.neighbors import KNeighborsClassifier
+from rank_bm25 import BM25Okapi
+from gen_embeds import collection
 
 class Retrieval:
     def knn_search(self, query_vector, k):
         # chromadb implementation
-        pass
+        collection.query(
+            query_vector=query_vector,
+            n_results=k,
+        )
 
     def bm25_rank(self, query_text, k):
         # rank_bm25 implementation
