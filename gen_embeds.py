3626b2cd50e6821e0fead694e1528558a80d2720
sathviknookala <sathviknookala@gmail.com>
2025-10-01 00:39:56 -0400

    finishing create_corpus() method instead for bm25 engine creation

--- gen_embeds.py
+++ gen_embeds.py
@@ -1,20 +1,34 @@
 from sentence_transformers import SentenceTransformer
+from rank_bm25 import BM25Okapi
 from get_user_profile import movie_file, tags_file, id_file
 import chromadb
 import pandas as pd
+import spacy
 import time
+import torch
 import math
 start_time = time.time()
+nlp = spacy.load("en_core_web_sm")
 
-bm25_engine = None
-def bm25_engine():
-    global bm25_engine
-    if bm25_engine is None:
-        movies_df = pd.read_csv('movie-info/movies.csv')
+print(torch.__version__)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(device)
 
-def create_collection(names_df: pd.DataFrame, tags_df: pd.DataFrame, k: int):
+def create_corpus(movie_df: pd.DataFrame, tags_df: pd.DataFrame, k: int):
+    '''
+    Initializes bm25 indexing engine
+    ''' 
+    corpus = []
+    for movie in movie_df[:k].itertuples():
+        movieId = movie.movieId
+
+
+def create_collection(collection_name: str, movie_df: pd.DataFrame, tags_df: pd.DataFrame, k: int):
+    '''
+    Initializes chromadb collection for KNN search
+    '''
     description_list = {}
-    for movie in names_df[:k].itertuples():
+    for movie in movie_df[:k].itertuples():
         movieId = movie.movieId
         tags = tags_df[tags_df['movieId'] == movieId]
         tag_text = ''.join(str(tags['tag'].tolist()))    
@@ -34,21 +48,19 @@ def create_collection(names_df: pd.DataFrame, tags_df: pd.DataFrame, k: int):
 
     client = chromadb.PersistentClient()
 
-    try:
-        collection = client.get_collection('rag_db')
-    except:
-        collection = client.create_collection(
-            name='rag_db',
-            configuration={
-                'hnsw': {
-                    'space': 'cosine',
-                    'max_neighbors': 16,
-                    'ef_construction': 200,
-                    'ef_search': 100,
-                }
-            }
-        )
-        print('Creating collection')
+
+    collection = client.create_collection(
+        name=collection_name,
+        configuration={
+            'hnsw': {
+                'space': 'cosine',
+                'max_neighbors': 16,
+                'ef_construction': 200,
+                'ef_search': 100,
+            } 
+        }
+    )
+    print('Creating collection')
 
     def batches(collection, embeddings, documents, ids, batch_size=5400):
         for index, i in enumerate(range(0, len(embeddings), batch_size), 1):
@@ -72,7 +84,7 @@ def create_collection(names_df: pd.DataFrame, tags_df: pd.DataFrame, k: int):
 
     return collection        
 
-collection = create_collection(movie_file, tags_file, 10000)
+collection = create_collection('rag_db', movie_file, tags_file, 10000)
 
 end_time = time.time()
 print(f"Time taken: {end_time-start_time}")
